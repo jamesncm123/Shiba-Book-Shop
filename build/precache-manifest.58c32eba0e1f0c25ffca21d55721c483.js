de7168985f6db8dede631bea2a7656956b010621
Yuttasak <yuttasak@tconio.com>
2021-02-24 00:06:21 +0700

    build

---
+++ build/precache-manifest.58c32eba0e1f0c25ffca21d55721c483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b86b6bd73279a90da4939381a5725e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40785dd099e5251f265f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c1623cb1569494e6cbf",
+    "url": "/static/js/2.4aebc05b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4aebc05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40785dd099e5251f265f",
+    "url": "/static/js/main.6ae9521f.chunk.js"
+  },
+  {
+    "revision": "2cbfc2cc99a962e45570",
+    "url": "/static/js/runtime-main.e6c36524.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
